35e3ec57aa2e61ff8fbdf5a9094c628b77c84390
Piols <pioispro@gmail.com>
2023-06-29 14:56:16 +0200

    indexed rows/columns

--- app/src/main/java/com/example/gridentify/MainActivity.kt
+++ app/src/main/java/com/example/gridentify/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
@@ -17,11 +18,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gridLayout: GridLayout = findViewById(R.id.gridLayout)
+
+        // Set click listener for each cell in the grid
+        for (row in 0 until gridLayout.rowCount) {
+            for (column in 0 until gridLayout.columnCount) {
+                val frameView = gridLayout.getChildAt(row * gridLayout.columnCount + column) as FrameLayout
+                val cellView = frameView.getChildAt(0) as Button
+                cellView.setOnClickListener { onCellClicked(cellView, row, column) }
+            }
+        }
     }
 
-    fun onCellClicked(view: View) {
+    fun onCellClicked(view: View, row: Int, column: Int) {
         val cellButton = view as Button
         cellButton.setBackgroundColor(Color.BLUE)
+
+        println("Clicked cell at row: $row, column: $column")
     }
 }
 
